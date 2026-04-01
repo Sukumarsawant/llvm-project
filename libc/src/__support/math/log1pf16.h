39bf9c98cd877260fce07997384c0632eb385264
Sukumarsawant <sawantsukumar@gmail.com>
2026-04-02 01:42:21 +0530

    skeleton for implementation

---
+++ libc/src/__support/math/log1pf16.h
@@ -0,0 +1,20 @@
+//===-- Implementation header for log1pf16 ----------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+namespace LIBC_NAMESPACE_DECL {
+
+namespace math {
+
+LIBC_INLINE static float log1pf(float x) {
+
+}
+
+} // namespace math
+} // namespace LIBC_NAMESPACE_DECL
+
+#endif // LLVM_LIBC_SRC___SUPPORT_MATH_LOG1PF_H
