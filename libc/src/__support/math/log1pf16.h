a3eeb970eaaf3628d395c4c3a5aa90397ad35079
Sukumarsawant <sawantsukumar@gmail.com>
2026-04-02 19:44:55 +0530

    test

--- libc/src/__support/math/log1pf16.h
+++ libc/src/__support/math/log1pf16.h
@@ -11,7 +11,6 @@
 
 #include "include/llvm-libc-macros/float16-macros.h"
 
-#ifdef LIBC_TYPES_HAS_FLOAT16
 
 #include "exp10_float16_constants.h"
 #include "expxf16_utils.h"
@@ -33,12 +32,51 @@ namespace LIBC_NAMESPACE_DECL {
 namespace math {
 
 LIBC_INLINE float16 log1pf(float16 x) {
+/*
+Points to consider near x = -1 , 0 
+Domain (1+x)>0
+        x>-1
+*/
+  using FPBits = typename fputil::FPBits<float16>;
+  FPBits xbits(x);
 
+  uint16_t x_u = xbits.uintval();
+  uint16_t x_abs = x_u & 0x7fff;
+  float x_sign = (x_u >> 15) ? -1 : 1;
+  float xf = x;
+
+/* 8000 = -0
+   7000 = +0
+   7C00 = +INF ( 0 11111 0000000000)
+   FC00 = -INF 
+   3C00 = +1
+   BC00 = -1
+*/
+
+  // Case : x+1<=0 => x<=-1
+  // or     x+1 == 1 => x ==0
+  // Case : -1<=x<=0 
+  if(x_u <= 0x8000U && x_u > INF ){
+    // log1p(+/- 0) = log(1 + (+/- 0)) = +/- 0 
+    if (x_abs == 0U)
+      return x;
+    // x = -1 => y = log1p(x) = -inf
+    if(LIBC_UNLIKELY(x_u==0xBC00U)){
+        fputil::raise_except_if_required(FE_DIVBYZERO);
+        return fputil::cast<float16>(0x7C00);
+    }
+
+    // use table
+  }
+
+  // Case 0<x<+INF
+  if(x_u < 0xBC00U){
+
+  }
 }
 
 } // namespace math
 } // namespace LIBC_NAMESPACE_DECL
 
-#endif // LIBC_TYPES_HAS_FLOAT16
 
 #endif // LLVM_LIBC_SRC___SUPPORT_MATH_LOG1PF_H
