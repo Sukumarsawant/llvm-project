5acd939bf29c5f73f141dc2720c38873c30fdcbf
Sukumarsawant <sawantsukumar@gmail.com>
2026-04-05 18:16:25 +0530

    skeleton for implementation

--- libc/test/shared/shared_math_test.cpp
+++ libc/test/shared/shared_math_test.cpp
@@ -481,6 +481,8 @@ TEST(LlvmLibcSharedMathTest, AllBFloat16) {
                                                          bfloat16(0.0)));
   EXPECT_FP_EQ(bfloat16(0.0), setpayloadsigbf16_res);
 
+  EXPECT_FP_EQ(bfloat16(0.0), LIBC_NAMESPACE::shared::tanpibf16(bfloat16(0.0)));
+
   EXPECT_FP_EQ(bfloat16(0.0), LIBC_NAMESPACE::shared::log_bf16(bfloat16(1.0)));
 
   bfloat16 neg_min_denormal = FPBits::min_subnormal(Sign::NEG).get_val();
